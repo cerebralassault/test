f11d424ebe33e2137d5ea47208fa13cce690b462
cerebralassault <imgmorley@gmail.com>
2026-04-01 21:29:33 +0000

    Update SSL connection to use www.microsoft.com

--- test3.ps1
+++ test3.ps1
@@ -1,32 +1,8 @@
-$uri = 'tas02.sls.update.microsoft.com'
-$tcp = New-Object Net.Sockets.TcpClient($uri, 443)
+$tcp = New-Object Net.Sockets.TcpClient('www.microsoft.com', 443)
 $ssl = New-Object Net.Security.SslStream($tcp.GetStream())
 try {
-    $ssl.AuthenticateAsClient($uri)
-    Write-Host "Protocol: $($ssl.SslProtocol) Cipher: $($ssl.CipherAlgorithm) Strength: $($ssl.CipherStrength)"
-} catch {
-    Write-Host "FAILED: $($_.Exception.Message)"
-    if ($_.Exception.InnerException) { Write-Host "Inner: $($_.Exception.InnerException.Message)" }
-} finally {
-    $ssl.Close()
-    $tcp.Close()
-}
-
-
-$callback = [Net.Security.RemoteCertificateValidationCallback]{
-    param($sender, $cert, $chain, $errors)
-    Write-Host "Subject: $($cert.Subject)"
-    Write-Host "Issuer: $($cert.Issuer)"
-    Write-Host "Errors: $errors"
-    return $true
-}
-
-$uri = 'tas02.sls.update.microsoft.com'
-$tcp = New-Object Net.Sockets.TcpClient($uri, 443)
-$ssl = New-Object Net.Security.SslStream($tcp.GetStream(), $false, $callback)
-try {
-    $ssl.AuthenticateAsClient($uri)
-    Write-Host "Protocol: $($ssl.SslProtocol)"
+    $ssl.AuthenticateAsClient('www.microsoft.com')
+    Write-Host "OK - Protocol: $($ssl.SslProtocol)"
 } catch {
     Write-Host "FAILED: $($_.Exception.Message)"
 } finally {
