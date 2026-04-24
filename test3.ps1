ccf69f3014955732e7bd82415c0de12ad8cdbe77
cerebralassault <imgmorley@gmail.com>
2026-04-24 16:56:56 +0000

    Update test3.ps1 with Azure Arc actions
    Added read-only actions and description for Azure Arc-enabled SQL Server instances.

--- test3.ps1
+++ test3.ps1
@@ -1,14 +1,40 @@
-$tcp = New-Object Net.Sockets.TcpClient('www.microsoft.com', 443)
-$ssl = New-Object Net.Security.SslStream($tcp.GetStream())
-try {
-    $ssl.AuthenticateAsClient('www.microsoft.com')
-    Write-Host "OK - Protocol: $($ssl.SslProtocol)"
-} catch {
-    Write-Host "FAILED: $($_.Exception.Message)"
-} finally {
-    $ssl.Close()
-    $tcp.Close()
-}
+    "Description": "Read-only viewer for Arc-enabled SQL Server instances, Arc-connected host machines, performance telemetry, patch history, metrics, resource health, and policy compliance.",
+    "Actions": [
+        "Microsoft.AzureArcData/sqlServerInstances/read",
+        "Microsoft.AzureArcData/sqlServerInstances/databases/read",
+        "Microsoft.AzureArcData/sqlServerInstances/availabilityGroups/read",
+        "Microsoft.AzureArcData/sqlServerInstances/getTelemetry/action",
+        "Microsoft.AzureArcData/locations/operationStatuses/read",
+        "Microsoft.AzureArcData/operations/read",
 
+        "Microsoft.HybridCompute/machines/read",
+        "Microsoft.HybridCompute/machines/extensions/read",
+        "Microsoft.HybridCompute/machines/hybridIdentityMetadata/read",
+        "Microsoft.HybridCompute/machines/patchAssessmentResults/read",
+        "Microsoft.HybridCompute/machines/patchAssessmentResults/softwarePatches/read",
+        "Microsoft.HybridCompute/machines/patchInstallationResults/read",
+        "Microsoft.HybridCompute/machines/patchInstallationResults/softwarePatches/read",
 
-Get-TlsCipherSuite | Where-Object { $_.Protocols -contains 'TLS1.2' -or $_.Protocols -contains 'TLS1.3' } | Select-Object Name | Format-Table -AutoSize
+        "Microsoft.Insights/metrics/read",
+        "Microsoft.Insights/metricDefinitions/read",
+        "Microsoft.Insights/diagnosticSettings/read",
+        "Microsoft.Insights/diagnosticSettingsCategories/read",
+        "Microsoft.Insights/logDefinitions/read",
+
+        "Microsoft.ResourceHealth/availabilityStatuses/read",
+        "Microsoft.ResourceHealth/events/read",
+        "Microsoft.ResourceHealth/events/impactedResources/read",
+        "Microsoft.ResourceHealth/impactedResources/read",
+
+        "Microsoft.ResourceGraph/resources/read",
+
+        "Microsoft.Resources/subscriptions/resourceGroups/read",
+        "Microsoft.Resources/subscriptions/resources/read",
+        "Microsoft.Resources/tags/read",
+        "Microsoft.Resources/subscriptions/operationresults/read",
+
+        "Microsoft.Authorization/policyAssignments/read",
+        "Microsoft.Authorization/policyDefinitions/read",
+        "Microsoft.Authorization/policySetDefinitions/read",
+        "Microsoft.Authorization/permissions/read"
+    ],
